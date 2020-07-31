b5cd0500d134c0f2bdd658a171f8a4408d2243a4
Artem <Tkachuk>
2020-07-31 16:39:50 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Artem Tkachuk on 7/31/20.
+//  Copyright © 2020 Artem Tkachuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
